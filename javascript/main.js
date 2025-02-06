709ac6bf8f11e88fc424b2d8c7717a1d4bf7154f
Alief <etaanomore@gmail.com>
2025-02-06 11:00:22 +0700

    first commit

---
+++ javascript/main.js
@@ -0,0 +1,21 @@
+
+// const nama = 'musaabd'
+// let umur = 20
+// let pekerjaan = true
+// let tinggiBadan = 175
+
+// console.log(nama)
+// console.log(umur)
+
+// console.log(typeof nama)
+// console.log(typeof umur)
+// console.log(typeof pekerjaan)
+// console.log(typeof tinggiBadan)
+
+let angka = prompt('masukkan angka?')
+
+if(angka % 2 == 0){
+    alert(angka + ' adalah bilangan genap')
+}else{
+    alert(angka + ' adalah bilangan ganjil')
+} //modulus % sisa bagi 
